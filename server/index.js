3bf5a4973db59d5adcad15fb54b10f02c062ac78
Yahor Anishchanka <anishegor@gmail.com>
2018-10-13 18:35:33 +0300

    fix listen server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,7 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/index.html'));
-});
-
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../dist/index.html')));
 app.use(express.static('dist'));
-
-app.listen(4500, () => {
-    console.log('Example app listening on port 4500!');
-});
+app.listen(4500, () => console.log('Server start at: http://localhost:4500'));
